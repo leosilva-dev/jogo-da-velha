6137c52ca40d7d1c90a51ceb46c1fa9c7413fa13
Leonardo Silva <silvaleonardo.ti@gmail.com>
2021-03-21 10:34:55 -0300

    Criando jogadores a partir do nome que o usuário informar

--- src/App.java
+++ src/App.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String nome = "";
+
+        System.out.println("Jogo da velha!");
+
+        while(nome.isEmpty()){
+            System.out.println("Informe o nome do primeiro jogador: ");
+            nome = sc.nextLine();
+        }
+
+        Jogador j1 = new Jogador(nome);
+
+        nome = "";
+        while(nome.isEmpty()){
+            System.out.println("Informe o nome do segundo jogador: ");
+            nome = sc.nextLine();
+        }
+
+        Jogador j2 = new Jogador(nome);
+
+        
+        
+
+
+        sc.close();
     }
 }
